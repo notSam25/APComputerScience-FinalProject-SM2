8af8e3cd48d4ed9ea5c2de650e7bccc498024335
Samuel Wiseman <dontpeak@github.com>
2023-04-03 22:20:23 -0500

    add basic tools, gave player hands

--- src/Entities/Player.java
+++ src/Entities/Player.java
@@ -2,9 +2,12 @@ package Entities;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
 
+import Entities.Tools.Spear;
+import Entities.Tools.Tool.TOOL_RARITY;
 import GFX.Camera;
 import GFX.Renderer;
 import Game.Map;
@@ -84,60 +87,66 @@ public class Player implements Entity {
 
         handleCamera();
     }
-    /* 
-    public Point calculateCircumferencePoint(int theta, int centerX, int centerY, int radius) {
-        double radians = Math.toRadians(theta);
-        int x = centerX + (int) Math.round(Math.cos(radians) * radius);
-        int y = centerY + (int) Math.round(Math.sin(radians) * radius);
-        return new Point(x, y);
-    }*/
 
-    private int theta = 0;
-
-    public void onRender(Graphics g) {
-        /*  test
-        theta++;
-        theta = (theta >= 360) ? 0 : theta;
-
-        Point center = calculateCircumferencePoint(theta, screenPosition[0] + (Tile.tileWidth + Tile.tileGap) / 2, screenPosition[1]+ (Tile.tileWidth + Tile.tileGap) / 2, 10);
-        int radius = 10;
-        int diameter = radius * 2;
-        g.setColor(Color.blue);
-        g.fillOval(center.x - radius, center.y - radius,
-                diameter, diameter);
-        g.drawString("Theta: " + theta, 10, Renderer.getWindowHeight() - 30);
-        // test
-
-        */
-
-        int mouse_x = Renderer.getMouseScreenPosition()[0], mouse_y = Renderer.getMouseScreenPosition()[1];
+    private void drawPlayerHands(Graphics g, int mouse_x, int mouse_y) {
         double circleX = mouse_x - screenPosition[0], circleY = mouse_y - screenPosition[1];
 
         double distance =  Math.sqrt((Math.pow(circleX, 2)) + Math.pow(circleY, 2));
         circleX /= distance;
         circleY /= distance;
+        
+        circleX *= 15;
+        circleY *= 15;
+        
+        double theta = Math.atan2(circleY, circleX);
 
-        circleX *= 50;
-        circleY *= 50;
+        double distance2 = 20.0;
+        double offset = (Math.PI / 180) * 45.0;
+        
+        double leftHandX = distance2 * Math.cos(theta + offset);
+        double leftHandY = distance2 * Math.sin(theta + offset);
 
-        System.out.printf("circleX: (%f, %f)\n", circleX, circleY);
+        double rightHandX = distance2 * Math.cos(theta - offset);
+        double rightHandY = distance2 * Math.sin(theta - offset);
+        
+        // draw the tool
+        this.tool.setLeftHand(new int[]{screenPosition[0] + (int)leftHandX, screenPosition[1] + (int)leftHandY});
+        this.tool.setRightHand(new int[]{screenPosition[0] + (int)rightHandX, screenPosition[1] + (int)rightHandY});
+        
+        this.tool.onRender(g);
 
-        g.setColor(Color.red);
-        g.fillOval(screenPosition[0] + (int)circleX - 10, screenPosition[1] + (int)circleY - 10, 20, 20);
+        // right hand
+        g.setColor(outlineColor);
+        g.fillOval((int)(screenPosition[0] + (int)leftHandX - (handSize * 1.6) / 2), (int)(screenPosition[1] + (int)leftHandY - (handSize * 1.6) / 2), (int)(handSize * 1.6), (int)(handSize * 1.6));
+        
+        g.setColor(skinColor);
+        g.fillOval(screenPosition[0] + (int)leftHandX - handSize / 2, screenPosition[1] + (int)leftHandY - handSize / 2, handSize, handSize);
 
-        g.setColor(Color.black);
+        // left hand
+        g.setColor(outlineColor);
+        g.fillOval((int)(screenPosition[0] + (int)rightHandX - (handSize * 1.6) / 2), (int)(screenPosition[1] + (int)rightHandY - (handSize * 1.6) / 2), (int)(handSize * 1.6), (int)(handSize * 1.6));
+        
+        g.setColor(skinColor);
+        g.fillOval(screenPosition[0] + (int)rightHandX - handSize / 2, screenPosition[1] + (int)rightHandY - handSize / 2, handSize, handSize);
+    }
+
+    public void onRender(Graphics g) {
+        drawPlayerHands(g, Renderer.getMouseScreenPosition()[0], Renderer.getMouseScreenPosition()[1]);
+
+        // draw the player circles
+        g.setColor(outlineColor);
         g.fillOval(screenPosition[0] - (Tile.tileWidth + (Tile.tileGap * 3)) / 2, screenPosition[1] - (Tile.tileWidth + (Tile.tileGap * 3)) / 2,
                 Tile.tileWidth + (Tile.tileGap * 3), Tile.tileHeight + (Tile.tileGap * 3));
 
         g.setColor(skinColor);
         g.fillOval(screenPosition[0] - (Tile.tileWidth + Tile.tileGap) / 2, screenPosition[1] - (Tile.tileWidth + Tile.tileGap) / 2, Tile.tileWidth + Tile.tileGap, Tile.tileHeight + Tile.tileGap);
-        // System.out.println("ScreenPos: " + screenPosition[0] + " | " +
-        // screenPosition[1]);
-        // System.out.println("WorldPos: " + worldPosition[0] + " | " + worldPosition
-        // [1]);
     }
 
-    private static Color skinColor = new Color(252, 200, 117, 255);
+    private Spear tool = new Spear(TOOL_RARITY.STONE);
+
+    private static int handSize = 10;
+
+    public static Color skinColor = new Color(252, 200, 117, 255), outlineColor = new Color(51, 51, 51, 255);
 
     private int[] screenPosition;
     private int[] worldPosition;
